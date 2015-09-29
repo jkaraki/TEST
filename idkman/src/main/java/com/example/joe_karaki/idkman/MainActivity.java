d34ee6066cb092c147f045a5e2c961497da49505
Joe Karaki <j.karaki98@gmail.com>
2015-09-28 20:28:10 -0400

    working on day 4

--- idkman/src/main/java/com/example/joe_karaki/idkman/MainActivity.java
+++ idkman/src/main/java/com/example/joe_karaki/idkman/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.joe_karaki.idkman;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,21 +10,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
+
+
 //hey everyone
 
 public class MainActivity extends AppCompatActivity {
-    int br;
     int bl;
+    int br;
     int tr;
     int tl;
+
+
 
     @Override
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_day3_2);
-        Button button1 = (Button) findViewById(R.id.button10);
+        setContentView(R.layout.layout_day4);
+        Button button1 = (Button) findViewById(R.id.button21);
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 tl++;
@@ -31,18 +38,30 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("onCreate", "Top Left Pressed");
                 Log.i("onCreate", String.valueOf(tl));
 
+                SharedPreferences mSettings = MainActivity.this.getSharedPreferences("Settings", 0);
+                SharedPreferences.Editor editor = mSettings.edit();
+                editor.putInt("tl", tl);
+                editor.commit();
+
+
             }
         });
-        Button button2 = (Button) findViewById(R.id.button11);
+        Button button2 = (Button) findViewById(R.id.button22);
         button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 tr++;
                 Toast.makeText(MainActivity.this, String.valueOf(tr), Toast.LENGTH_SHORT).show();
                 Log.i("onCreate", "Top Right Pressed");
                 Log.i("onCreate", String.valueOf(tr));
+
+                SharedPreferences mSettings = MainActivity.this.getSharedPreferences("Settings", 0);
+                SharedPreferences.Editor editor = mSettings.edit();
+                editor.putInt("tr", tr);
+                editor.commit();
+
             }
         });
-        Button button3 = (Button) findViewById(R.id.button19);
+        Button button3 = (Button) findViewById(R.id.button23);
         button3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 bl++;
@@ -50,16 +69,38 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("onCreate", "Bottom Left Pressed");
                 Log.i("onCreate", String.valueOf(bl));
 
+                SharedPreferences mSettings = MainActivity.this.getSharedPreferences("Settings", 0);
+                SharedPreferences.Editor editor = mSettings.edit();
+                editor.putInt("bl", bl);
+                editor.commit();
 
             }
         });
-        Button button4 = (Button) findViewById(R.id.button20);
+        Button button4 = (Button) findViewById(R.id.button24);
         button4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 br++;
                 Toast.makeText(MainActivity.this, String.valueOf(br), Toast.LENGTH_SHORT).show();
                 Log.i("onCreate", "Bottom Right Pressed");
                 Log.i("onCreate", String.valueOf(br));
+                SharedPreferences mSettings = MainActivity.this.getSharedPreferences("Settings", 0);
+                SharedPreferences.Editor editor = mSettings.edit();
+                editor.putInt("tl", tl);
+                editor.commit();
+
+            }
+        });
+        Button reset = (Button) findViewById(R.id.button25);
+        reset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SharedPreferences mSettings = MainActivity.this.getSharedPreferences("Settings", 0);
+                SharedPreferences.Editor editor = mSettings.edit();
+                editor.putInt("tl", 0);
+                editor.putInt("tr", 0);
+                editor.putInt("bl", 0);
+                editor.putInt("br", 0);
+
+                editor.commit();
 
             }
         });
